d33bcde4221f86f64eb0a2995aa83c08f2666720
Clearlove7 <88828156+tuongclearlove7@users.noreply.github.com>
2023-04-11 13:27:56 +0700

    update code

--- websocket.io/webchat/src/services/getDataClient.js
+++ websocket.io/webchat/src/services/getDataClient.js
@@ -3,10 +3,12 @@ function userConnectServer(io){
     io.on('connection', (socket) => {
         
         console.log(`${socket.id} user connect server...`);
+        //user disconnect server
         socket.on('disconnect', () => {
             console.log(`${socket.id} disconnect server`);
         });
 
+        //server giao tiep voi client
         socket.emit("server","receive from server to client");
     });
 }
