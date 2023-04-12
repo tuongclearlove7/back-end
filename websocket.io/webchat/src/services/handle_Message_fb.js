46d1a28694ad4965599c994e3b8096ec89485e46
Clearlove7 <88828156+tuongclearlove7@users.noreply.github.com>
2023-04-13 00:59:55 +0700

    update code

--- websocket.io/webchat/src/services/handle_Message_fb.js
+++ websocket.io/webchat/src/services/handle_Message_fb.js
@@ -3,15 +3,15 @@ const {callSendAPI} =  require("../controller/botFbController");
 function handleMessage(sender_psid, received_message) {
     let response;        
     if (received_message.text) {    
-
-        response = {
-          "text": `${received_message.text}`,
-        }
-    }
    
+       response = {
+        "text": `${received_message.text}`,
+      }
+  }
+
   if(received_message.text==="#Tường"){
     response = {"text": `${received_message.text} : Tường nick name Clearlove7 Profile : `+SearchBot[36],
-  }
+    }
 }
 
    else if (received_message.attachments) {
